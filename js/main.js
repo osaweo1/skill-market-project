36d1dfd611bdc53aa9662fc9122b9ae7a50434af
osaweo1 <osamudiamenosawe@gmail.com>
2022-04-14 17:52:17 +0100

    update on this

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const menuBtn = document.querySelector('.menu')
 const navClass = document.querySelector('.nav')
 const cancelBtn = document.querySelector('.cancel')
-
-
+const navigation = document.querySelector('.nav');
+const body = document.querySelector('body');
 
 
 
 
 
 menuBtn.addEventListener('click', function() {
-    navClass.style.transform = "translate(0%)"
-    menuBtn.style.display = "none"
+    navClass.style.transform = "translate(0%)";
+    menuBtn.style.display = "none";
 })
 
 
